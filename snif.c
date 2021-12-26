aa2ac67f40f3970484455963e9b06489dad091b5
Yanir <66729230+yanir75@users.noreply.github.com>
2021-12-26 15:37:17 +0200

    Update snif.c

--- snif.c
+++ snif.c
@@ -14,22 +14,23 @@
 
 
 /* IP header */
-typedef struct sniff_ip
+typedef struct sniff_ip 
 {
-	unsigned ip_vhl;			   /* version << 4 | header length >> 2 */
-	unsigned ip_tos;			   /* type of service */
-	unsigned ip_len;			   /* total length */
-	unsigned ip_id;				   /* identification */
-	unsigned ip_off;			   /* fragment offset field */
-#define IP_RF 0x8000			   /* reserved fragment flag */
-#define IP_DF 0x4000			   /* dont fragment flag */
-#define IP_MF 0x2000			   /* more fragments flag */
-#define IP_OFFMASK 0x1fff		   /* mask for fragmenting bits */
-	unsigned ip_ttl;			   /* time to live */
-	unsigned ip_p;				   /* protocol */
-	unsigned short ip_sum;		   /* checksum */
-	struct in_addr ip_src, ip_dst; /* source and dest address */
-}iph;
+        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+        u_char  ip_tos;                 /* type of service */
+        u_short ip_len;                 /* total length */
+        u_short ip_id;                  /* identification */
+        u_short ip_off;                 /* fragment offset field */
+        #define IP_RF 0x8000            /* reserved fragment flag */
+        #define IP_DF 0x4000            /* dont fragment flag */
+        #define IP_MF 0x2000            /* more fragments flag */
+        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+        u_char  ip_ttl;                 /* time to live */
+        u_char  ip_p;                   /* protocol */
+        u_short ip_sum;                 /* checksum */
+        struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+iph;
 #define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip) (((ip)->ip_vhl) >> 4)
 
