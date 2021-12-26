1049804afcfb06e9bc4f67813e1bfa69755f0abb
Yanir <66729230+yanir75@users.noreply.github.com>
2021-12-26 14:22:23 +0200

    Update snif.c

--- snif.c
+++ snif.c
@@ -1,77 +1,30 @@
 #include <pcap.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <stdlib.h>
-#define SIZE_ETHERNET 14
-#define PCKT_LEN 1024
-#define IP_LEN 20
+#include <stdio.h>
 
-/* IP header */
-struct sniff_ip
+void got_packet(u_char *args, const struct pcap_pkthdr *header, 
+        const u_char *packet)
 {
-	unsigned ip_vhl;			   /* version << 4 | header length >> 2 */
-	unsigned ip_tos;			   /* type of service */
-	unsigned ip_len;			   /* total length */
-	unsigned ip_id;				   /* identification */
-	unsigned ip_off;			   /* fragment offset field */
-	#define IP_RF 0x8000			   /* reserved fragment flag */
-	#define IP_DF 0x4000			   /* dont fragment flag */
-	#define IP_MF 0x2000			   /* more fragments flag */
-	#define IP_OFFMASK 0x1fff		   /* mask for fragmenting bits */
-	unsigned ip_ttl;			   /* time to live */
-	unsigned ip_p;				   /* protocol */
-	unsigned short ip_sum;		   /* checksum */
-	struct in_addr ip_src, ip_dst; /* source and dest address */
-};
-#define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
-#define IP_V(ip) (((ip)->ip_vhl) >> 4)
-
-struct sniff_icmp{
-	#define ICMP_ECHO_REQ 8
-	#define ICMP_ECHO_RES 0
-	#define ICMP_HDR_LEN 4
- 	unsigned char icmp_type;
- 	unsigned char icmp_code;
- 	unsigned short icmp_cksum;		/* icmp checksum */
- 	unsigned short icmp_id;				/* icmp identifier */
- 	unsigned short icmp_seq;			/* icmp sequence number */
-};
-
-void got_packet(u_char *args, const struct pcap_pkthdr *header,const u_char *handle){
-    struct sniff_ip* ip;
-    struct sniff_icmp* icmp;
-    ip = (struct sniff_ip*)(handle+14);
-    printf("%s",inet_ntoa(ip->ip_src));
-	printf("%s",inet_ntoa(ip->ip_dst));
-    printf("Got a packet");
+   printf("Got a packet\n");
 }
 
 int main()
 {
-    pcap_t *handle;
-    char errbuff[PCAP_ERRBUF_SIZE];
-    //char filter_exp[] = "ip proto ICMP";
-    struct bpf_program filter;
-    bpf_u_int32 net;
-    
-    handle = pcap_open_live("eth0",PCKT_LEN,1,1000,errbuff);
-    if(handle == NULL)
-    {
-        printf("%s\n",errbuff);
-        exit(1);
-    }
-    if(pcap_compile(handle,&filter,"icmp",0,net)==-1)
-        {
-            printf("bad filter\n");
-            exit(1);
-        }
-    if(pcap_setfilter(handle,&filter)==-1)
-    {
-        printf("failed to set filter");
-        exit(1);
-    }
-    pcap_loop(handle,0,got_packet,NULL);
-    return 0;
+  pcap_t *handle;
+  char errbuf[PCAP_ERRBUF_SIZE];
+  struct bpf_program fp;
+  char filter_exp[] = "ip proto icmp";
+  bpf_u_int32 net;
 
+  // Step 1: Open live pcap session on NIC with name eth3
+  handle = pcap_open_live("eth3", BUFSIZ, 1, 1000, errbuf); 
+
+  // Step 2: Compile filter_exp into BPF psuedo-code
+  pcap_compile(handle, &fp, filter_exp, 0, net);      
+  pcap_setfilter(handle, &fp);                             
+
+  // Step 3: Capture packets
+  pcap_loop(handle, -1, got_packet, NULL);                
+
+  pcap_close(handle);   //Close the handle 
+  return 0;
 }
