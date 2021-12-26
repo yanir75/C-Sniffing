956d51dada1d618e79285823dde0c1f3213406f5
Yanir <66729230+yanir75@users.noreply.github.com>
2021-12-26 15:31:53 +0200

    Update snif.c

--- snif.c
+++ snif.c
@@ -1,46 +1,41 @@
-#include <pcap.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netinet/ether.h>
+#include <linux/if_packet.h>
+#ifndef SIZE_ETHERNET
 #define SIZE_ETHERNET 14
-#define ETHER_ADDR_LEN 6
-#define PCKT_LEN 1518
-#define IP_LEN 20
+#endif
+#define PCKT_LEN 1024
 
 
-/* Ethernet addresses are 6 bytes */
-/* ethernet headers are always exactly 14 bytes  */
 
-/* Ethernet header */
-struct sniff_ethernet
-{
-	unsigned char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
-	unsigned char ether_shost[ETHER_ADDR_LEN];	  /* Source host address */
-	unsigned char ether_type;					   /* IP? ARP? RARP? etc */
-};
+
 
 /* IP header */
-struct sniff_ip
+typedef struct sniff_ip
 {
 	unsigned ip_vhl;			   /* version << 4 | header length >> 2 */
 	unsigned ip_tos;			   /* type of service */
 	unsigned ip_len;			   /* total length */
 	unsigned ip_id;				   /* identification */
 	unsigned ip_off;			   /* fragment offset field */
-	#define IP_RF 0x8000			   /* reserved fragment flag */
-	#define IP_DF 0x4000			   /* dont fragment flag */
-	#define IP_MF 0x2000			   /* more fragments flag */
-	#define IP_OFFMASK 0x1fff		   /* mask for fragmenting bits */
+#define IP_RF 0x8000			   /* reserved fragment flag */
+#define IP_DF 0x4000			   /* dont fragment flag */
+#define IP_MF 0x2000			   /* more fragments flag */
+#define IP_OFFMASK 0x1fff		   /* mask for fragmenting bits */
 	unsigned ip_ttl;			   /* time to live */
 	unsigned ip_p;				   /* protocol */
 	unsigned short ip_sum;		   /* checksum */
 	struct in_addr ip_src, ip_dst; /* source and dest address */
-};
+}iph;
 #define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip) (((ip)->ip_vhl) >> 4)
 
-struct sniff_icmp{
+
+
+typedef struct sniff_icmp{
 	#define ICMP_ECHO_REQ 8
 	#define ICMP_ECHO_RES 0
 	#define ICMP_HDR_LEN 4
@@ -49,46 +44,35 @@ struct sniff_icmp{
  	unsigned short icmp_cksum;		/* icmp checksum */
  	unsigned short icmp_id;				/* icmp identifier */
  	unsigned short icmp_seq;			/* icmp sequence number */
-};
-void got_packet(u_char *args, const struct pcap_pkthdr *header, 
-        const u_char *packet);
-int main()
+}icmph;
+
+void got_packet(unsigned char* buffer, int size)
 {
-  pcap_t *handle;
-  char errbuf[PCAP_ERRBUF_SIZE];
-  struct bpf_program fp;
-  char filter_exp[] = "ip proto ICMP";
-  bpf_u_int32 net;
+    iph *ip;
+    ip= (iph*)(buffer+SIZE_ETHERNET);
+    printf("src:%s\ndst:%s\n",inet_ntoa(ip->ip_src),inet_ntoa(ip->ip_dst));
 
-  // Step 1: Open live pcap session on NIC with name eth3
-  handle = pcap_open_live("any", PCKT_LEN, 1, 1000, errbuf); 
 
-  // Step 2: Compile filter_exp into BPF psuedo-code
-  pcap_compile(handle, &fp, filter_exp, 0, net);      
-  pcap_setfilter(handle, &fp);                             
-
-  // Step 3: Capture packets
-  pcap_loop(handle, -1, got_packet, NULL);                
-
-  pcap_close(handle);   //Close the handle 
-  return 0;
+    
 }
 
+int main(int argc, char *argv[]) {
 
+    int sock;
+    if ((sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1) {
+        perror("could not create socket");
+        return -1;
+    }
 
-void got_packet(u_char *args, const struct pcap_pkthdr *header, 
-        const u_char *packet)
-{
- 	struct sniff_ethernet* eth;
-	struct sniff_ip* ip;
-	struct sniff_icmp* icmp;
-	eth = (struct sniff_ethernet*)(packet);
-	ip=(struct sniff_ip*)(packet+SIZE_ETHERNET);
-	icmp = (struct sniff_icmp*)(packet+SIZE_ETHERNET+IP_LEN);
-	printf("%s\n",inet_ntoa(ip->ip_src));
-	printf("%s\n",inet_ntoa(ip->ip_dest));
-	printf("%d\n",icmp->icmp_type);
-	printf("%d\n",icmp->icmp_code);
+    struct packet_mreq mr;
+    mr.mr_type = PACKET_MR_PROMISC;
+    setsockopt(sock, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mr, sizeof(mr));
+    struct sockaddr dest_in;
+    socklen_t len = sizeof(dest_in);
+    char buf[1024];
+    while(1) {
+        bzero(buffer, 1024);
+        int rc = recvfrom(sock, buf, ETH_FRAME_LEN, 0, &dest_in, &len);
+        got_packet(buf, rc);
+    }
 }
-
-
