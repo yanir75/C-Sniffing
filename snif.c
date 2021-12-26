b356c6e4382a2ad9512afa7fa3b17c6c9aec51be
Yanir <66729230+yanir75@users.noreply.github.com>
2021-12-26 14:34:24 +0200

    Update snif.c

--- snif.c
+++ snif.c
@@ -1,10 +1,38 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <arpa/inet.h>
+
+/* IP Header */
+typedef struct ipheader {
+  unsigned char      iph_ihl:4, //IP header length
+                     iph_ver:4; //IP version
+  unsigned char      iph_tos; //Type of service
+  unsigned short int iph_len; //IP Packet length (data + header)
+  unsigned short int iph_ident; //Identification
+  unsigned short int iph_flag:3, //Fragmentation flags
+                     iph_offset:13; //Flags offset
+  unsigned char      iph_ttl; //Time to Live
+  unsigned char      iph_protocol; //Protocol type
+  unsigned short int iph_chksum; //IP datagram checksum
+  struct  in_addr    src; //Source IP address 
+  struct  in_addr    dst;   //Destination IP address 
+}iph;
+#define ETH_SIZE 14
+#define ETHER_ADDR_LEN 6
+/* Ethernet header */
+struct ethheader {
+  u_char  ether_dhost[ETHER_ADDR_LEN]; /* destination host address */
+  u_char  ether_shost[ETHER_ADDR_LEN]; /* source host address */
+  u_short ether_type;                  /* IP? ARP? RARP? etc */
+};
+
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, 
         const u_char *packet)
 {
-   printf("Got a packet\n");
+   iph* ip;
+   ip=(iph*)(packet+ETH_SIZE);
+   printf("SRC: %s\nDEST: %s\n",inet_ntoa(ip->src),inet_ntoa(ip->dst));
 }
 
 int main()
@@ -12,7 +40,7 @@ int main()
   pcap_t *handle;
   char errbuf[PCAP_ERRBUF_SIZE];
   struct bpf_program fp;
-  char filter_exp[] = "ip proto icmp";
+  char filter_exp[] = "ip proto ICMP";
   bpf_u_int32 net;
 
   // Step 1: Open live pcap session on NIC with name eth3
