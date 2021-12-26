4465f437a8593770bec245758870ab36c6b42f9c
Yanir <66729230+yanir75@users.noreply.github.com>
2021-12-26 15:08:28 +0200

    Update snif.c

--- snif.c
+++ snif.c
@@ -1,44 +1,56 @@
 #include <pcap.h>
-#include <stdio.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#define SIZE_ETHERNET 14
+#define ETHER_ADDR_LEN 6
+#define PCKT_LEN 1024
 
-/* IP Header */
-typedef struct sniff_ip
+
+/* Ethernet addresses are 6 bytes */
+/* ethernet headers are always exactly 14 bytes  */
+
+/* Ethernet header */
+struct sniff_ethernet
+{
+	unsigned char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+	unsigned char ether_shost[ETHER_ADDR_LEN];	  /* Source host address */
+	unsigned char ether_type;					   /* IP? ARP? RARP? etc */
+};
+
+/* IP header */
+struct sniff_ip
 {
 	unsigned ip_vhl;			   /* version << 4 | header length >> 2 */
 	unsigned ip_tos;			   /* type of service */
 	unsigned ip_len;			   /* total length */
 	unsigned ip_id;				   /* identification */
 	unsigned ip_off;			   /* fragment offset field */
-#define IP_RF 0x8000			   /* reserved fragment flag */
-#define IP_DF 0x4000			   /* dont fragment flag */
-#define IP_MF 0x2000			   /* more fragments flag */
-#define IP_OFFMASK 0x1fff		   /* mask for fragmenting bits */
+	#define IP_RF 0x8000			   /* reserved fragment flag */
+	#define IP_DF 0x4000			   /* dont fragment flag */
+	#define IP_MF 0x2000			   /* more fragments flag */
+	#define IP_OFFMASK 0x1fff		   /* mask for fragmenting bits */
 	unsigned ip_ttl;			   /* time to live */
 	unsigned ip_p;				   /* protocol */
 	unsigned short ip_sum;		   /* checksum */
 	struct in_addr ip_src, ip_dst; /* source and dest address */
-}iph;
+};
 #define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip) (((ip)->ip_vhl) >> 4)
-#define ETH_SIZE 14
-#define ETHER_ADDR_LEN 6
-/* Ethernet header */
-struct ethheader {
-  u_char  ether_dhost[ETHER_ADDR_LEN]; /* destination host address */
-  u_char  ether_shost[ETHER_ADDR_LEN]; /* source host address */
-  u_short ether_type;                  /* IP? ARP? RARP? etc */
+
+struct sniff_icmp{
+	#define ICMP_ECHO_REQ 8
+	#define ICMP_ECHO_RES 0
+	#define ICMP_HDR_LEN 4
+ 	unsigned char icmp_type;
+ 	unsigned char icmp_code;
+ 	unsigned short icmp_cksum;		/* icmp checksum */
+ 	unsigned short icmp_id;				/* icmp identifier */
+ 	unsigned short icmp_seq;			/* icmp sequence number */
 };
-
-
 void got_packet(u_char *args, const struct pcap_pkthdr *header, 
-        const u_char *packet)
-{
-   iph* ip;
-   ip=(iph*)(packet+ETH_SIZE);
-   printf("SRC: %s\nDEST: %s\n",inet_ntoa(ip->src),inet_ntoa(ip->dst));
-}
-
+        const u_char *packet);
 int main()
 {
   pcap_t *handle;
@@ -46,10 +58,9 @@ int main()
   struct bpf_program fp;
   char filter_exp[] = "ip proto ICMP";
   bpf_u_int32 net;
-  char * dev="eth0"
 
   // Step 1: Open live pcap session on NIC with name eth3
-  handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf); 
+  handle = pcap_open_live("any", PCKT_LEN, 1, 1000, errbuf); 
 
   // Step 2: Compile filter_exp into BPF psuedo-code
   pcap_compile(handle, &fp, filter_exp, 0, net);      
@@ -61,3 +72,22 @@ int main()
   pcap_close(handle);   //Close the handle 
   return 0;
 }
+
+
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, 
+        const u_char *packet)
+{
+ 	struct sniff_ethernet* eth;
+	struct sniff_ip* ip;
+	struct sniff_icmp* icmp;
+	eth = (struct sniff_ethernet*)(packet);
+	ip=(struct sniff_ip*)(packet+ETHERNET_SIZE);
+	icmp = (struct sniff_icmp*)(packet+ETHERNET_SIZE+20);
+	printf("%s"\n,inet_ntoa(ip->ip_src));
+	printf("%s"\n,inet_ntoa(ip->ip_dest));
+	printf("%d"\n,icmp->icmp_type);
+	printf("%d"\n,icmp->icmp_code);
+}
+
+
